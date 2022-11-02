4cea6cea773f6ade5eee0f8df961803dc2ddd4bf
Almudena Fernandez <fernandezalmuclases@gmail.com>
2022-11-02 10:52:51 +0100

    uso de estilos y flag para no poder volver atras

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -9,24 +9,33 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-    TextView textosingup;
+    /*TextView textosingup;*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        textosingup = findViewById(R.id.signup);
+       /* textosingup = findViewById(R.id.signup);*/
 
-        Intent intent = getIntent();
+      /*  Intent intent = getIntent();
        textosingup.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {iniciarSignUp();
-            }
-        });
+            public void onClick(View view) {iniciarSignUp();}
+        });*/
     }
 
-    public void iniciarSignUp()
+    public void iniciarSignup(View v) {
+        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+       /* intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);*/ /*PARA QUE NO SE PUEDA IR HACIA ATRAS*/
+       /* intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);*/ /*PARA QUE NO SE PUEDA IR HACIA ATRAS*/
+
+        startActivity(intent);
+
+
+
+    }
+   /* public void iniciarSignUp()
     {
         Intent intent = new Intent( this, SignupActivity.class);
         startActivity(intent);
-    }
+    }*/
 }
